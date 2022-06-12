7eef5956f179e0dc0db95728ad5654c03378f358
João Vitor Espig <jotinha1300@gmail.com>
2022-06-12 20:13:13 -0300

    Added folder that teaches you how to use 'puts'

---
+++ 01-hello-world/README.md
@@ -0,0 +1,3 @@
+# 01 - Hello World!
+
+Here you will learn the basics of printing messages in the stdout (Standard Output, aka terminal)
---
+++ 01-hello-world/hello-world.cr
@@ -0,0 +1,11 @@
+# prints 'Hello World!' in stdout (Standard Output, aka terminal :P)
+
+# Puts means 'put string'
+puts "Hello World!"
+# puts("Hello World!"") also works!
+
+# Seconde way to do it
+p! "Hello World!" 
+# p!("Hello World!") also works
+# the purpouse of p! it will be more clear if you take a look at
+# the variables folder!
