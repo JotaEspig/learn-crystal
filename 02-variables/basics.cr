d9a2b6be15501ca4ebc83b59743fb729c53f3e46
João Vitor Espig <jotinha1300@gmail.com>
2022-06-12 22:18:56 -0300

    Added the concept of constants

--- 02-variables/basics.cr
+++ 02-variables/basics.cr
@@ -9,3 +9,9 @@ message = 2022
 
 puts message
 p! typeof(message)
+
+# You can create a constant with all upper case letter
+PI = 3.14159
+p! PI
+# You cannot reassign a constant
+# PI = 10 is not possible
