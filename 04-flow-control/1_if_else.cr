ec5f4008c4c6a013f713e35de5bd539856b5b956
João Vitor Espig <jotinha1300@gmail.com>
2022-06-13 22:18:32 -0300

    Added the last content about if that was missing

--- 04-flow-control/1_if_else.cr
+++ 04-flow-control/1_if_else.cr
@@ -1,3 +1,10 @@
+# Crystal has some operatores for boolean algebra:
+# a && b (AND)
+# a || b (OR)
+# !a (NOT)
+# a != b (XOR)
+# a == b 
+
 # You can use IFs statements this way with 'end' keyword in the final
 x = 10
 if x > 5
@@ -41,8 +48,40 @@ p! x
 x = 1 > 2 ? 3 : 4
 p! 4
 
-### 1. Get pessoa id from url
-#   2. get object from id
-#   3. Change value in the object (like.: pessoa.nome = "Zeke")
-#   4. commit (db.session.commit())
-#   5. send response to client
+a = 1 > 2 ? nil : -10
+
+# if var
+# it will check if a is true codition or not nil
+if a
+    p! a.abs 
+    # It's possible to use abs method since the value cannot
+    # be nil because of "if a" statement
+end
+
+# if var.is_a?
+# it will check if var is a <class>
+if a.is_a?(Number)
+    puts %("a" is a Number)
+else
+    puts %("a" is not a Number)
+end
+
+# Adding operator &&
+if a.is_a?(Number) && a.is_a?(Int32)
+    puts "OH YEAH BABY! \"a\" IS A NUMBER AND INT32"
+end
+
+# if responds_to?
+# it will check if variable 'has' the method specified
+if a.responds_to?(:abs)
+    puts "\"a\" responds to \":abs\""
+end
+
+# if var.nil?
+# checks if var is nil
+a = nil
+if a.nil?
+    exit # exit program
+else
+    puts "\"a\" is not nil"
+end
