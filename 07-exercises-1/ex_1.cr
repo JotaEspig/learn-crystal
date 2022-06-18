a76c3fdd46b08fd687f6c96b1c07c86728e98257
João Vitor Espig <jotinha1300@gmail.com>
2022-06-18 14:41:57 -0300

    added solution to the problem

--- 07-exercises-1/ex_1.cr
+++ 07-exercises-1/ex_1.cr
@@ -3,3 +3,11 @@
 # Define a function that takes two parameters and prints the
 # product of the two number in this format:
 # "The product of <x> and <y> is equal to => <result>"
+
+
+#       SOLUTION
+def multiply(x : Number, y : Number)
+    puts "The product of #{x} and #{y} is equal to => #{x*y}"
+end
+
+multiply 10, 10.9
