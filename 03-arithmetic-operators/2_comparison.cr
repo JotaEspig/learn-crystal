ebe4e7e5255eacece2369201516fbe69bfb35e5a
João Vitor Espig <jotinha1300@gmail.com>
2022-07-02 22:15:33 -0300

    fixed bad english

--- 03-arithmetic-operators/2_comparison.cr
+++ 03-arithmetic-operators/2_comparison.cr
@@ -6,8 +6,8 @@ p! 2 > 1, 2 >= 2, 9 < 10.9
 
 # Universal comparison operator (aka Spaceship) 
 # returns 0 if both variables are equal.
-# 1 if the first value is greater than the seconde one
-# -1 if the seconde value is greater than the first one
+# 1 if the first value is greater than the second one
+# -1 if the second value is greater than the first one
 p! 1 <=> 1,
     2 <=> 1,
     1 <=> 2
