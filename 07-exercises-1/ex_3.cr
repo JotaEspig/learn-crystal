4926eb9713255ad248c6972181afb14348519884
João Vitor Espig <jotinha1300@gmail.com>
2022-07-13 18:52:51 -0300

    resolved ex_3

--- 07-exercises-1/ex_3.cr
+++ 07-exercises-1/ex_3.cr
@@ -1,3 +1,13 @@
 #       EXERCISE 3
 # 
 # Define a power function. BUT you must not use '**'
+def power(base : Int, n : Int) : Int
+    product = 1
+    n.times do
+        product *= base
+    end
+
+    product
+end
+
+p power(ARGV[0].to_i, ARGV[1].to_i)
