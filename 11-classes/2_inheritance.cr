da25cbe7fe6364022b5a0a4f04f6020e5c9fcc45
João Vitor Espig <jotinha1300@gmail.com>
2022-07-12 21:08:40 -0300

    added keyword: 'super'

--- 11-classes/2_inheritance.cr
+++ 11-classes/2_inheritance.cr
@@ -1,17 +1,25 @@
 class Animal
     property age : UInt8
+    def initialize(@age : UInt8) end
+    def say
+        puts "hey hey, I'm #{@age}"
+    end
 end
 
 class Cat < Animal
-    def meow
+    def say
         puts "meow, I'm #{@age} years old"
     end
 end
 
 class Dog < Animal
-    def bark
+    def say
+        super
         puts "woof. I'm #{@age} years old"
     end
 end
 
 # Dog and Cat inherits 'age' from Animal
+Animal.new(1).say
+Cat.new(1).say
+Dog.new(1).say
